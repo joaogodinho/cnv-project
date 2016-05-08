648ae06a36f931d7c69c6c4b2520c98a5b6d39c0
joaogodinho <joaocfgodinho@gmail.com>
2016-05-08 23:15:57 +0000

    Tests made to gather data about primes and semiprimes factorization

--- src/test/java/pt/ulisboa/tecnico/cnv/test/IntFactorizationTest.java
+++ src/test/java/pt/ulisboa/tecnico/cnv/test/IntFactorizationTest.java
@@ -1,24 +1,28 @@
 package pt.ulisboa.tecnico.cnv.test;
 
 import org.junit.Test;
+import org.junit.Before;
 
 import java.math.BigInteger;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Date;
 
 import java.math.BigInteger;
 
 import pt.ulisboa.tecnico.cnv.factorization.IntFactorization;
 
 public class IntFactorizationTest {
+    private int limit = 10000;
+    private ArrayList<BigInteger> numbers;
 
-    //@Test
-    public void primes1() {
-        int limit = 100;
+    @Before
+    public void loadFile() {
         ClassLoader classLoader = getClass().getClassLoader();
-        ArrayList<BigInteger> numbers = new ArrayList<BigInteger>();
+        numbers = new ArrayList<BigInteger>();
         try (BufferedReader br = new BufferedReader(
                     new FileReader(classLoader.getResource("primes1.txt").getFile()))) {
 
@@ -45,9 +49,39 @@ limitReached:
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    //@Test
+    public void primes1() {
         if (numbers.size() > 0) {
             IntFactorization intFact;
             for (BigInteger bi: numbers) {
+                //System.out.println(bi.bitLength());
+                intFact = new IntFactorization();
+                intFact.calcPrimeFactors(bi);
+            }
+        } else {
+            System.out.println("No numbers to test.");
+        }
+    }
+
+    @Test
+    public void primes2() {
+        Random rand = new Random(new Date().getTime());
+        int tempLimit = limit;
+        if (numbers.size() > 0) {
+            ArrayList<BigInteger> semiprimes = new ArrayList<BigInteger>();
+            while (semiprimes.size() < limit) {
+                BigInteger semi = numbers.get(rand.nextInt(limit))
+                    .multiply(numbers.get(rand.nextInt(limit)));
+                //if (semi.bitLength() <= 17) {
+                    semiprimes.add(semi);
+                    System.out.println(semi.bitLength());
+                //}
+            }
+            assert semiprimes.size() == limit;
+            IntFactorization intFact;
+            for (BigInteger bi: semiprimes) {
                 intFact = new IntFactorization();
                 intFact.calcPrimeFactors(bi);
             }
